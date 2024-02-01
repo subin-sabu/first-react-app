08491fbbca0025ed9b4532e77749b8ace4145950
subin-sabu <subincs2023@gmail.com>
2024-02-01 18:08:52 +0530

    Added Hash Router replacing Browser Router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Shop } from './Pages/Shop';
 import { ShopCategory } from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
@@ -17,7 +17,7 @@ import kids_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div >
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop/>} />
@@ -32,7 +32,7 @@ function App() {
 
         </Routes>
         <Footer/>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
