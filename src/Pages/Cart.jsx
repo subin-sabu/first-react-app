31ea7d8be023f6185b1b24f3b23616bf0e253e79
subin-sabu <subincs2023@gmail.com>
2024-02-01 06:10:46 +0530

    finished basic design. Not responsive now

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CSS/Cart.css'
+import { CartItems } from '../Components/CartItems/CartItems'
+
+
+export const Cart = () => {
+  return (
+    <div>
+      <CartItems/>
+    </div>
+  )
+}
